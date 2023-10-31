4fdf4e7c55b4e863601b6e831165f41162bb2696
Gisela Benito Izquierdo <gisela.benito@enti.cat>
2023-10-31 18:05:32 +0100

    Jump Implementation

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,7 +9,13 @@ public class PlayerJump : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] private InputActionReference jumps;
+    [SerializeField] private float jumpPower;
 
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    
+    private bool isGrounded; 
+    private bool isJumping;
     private Rigidbody2D rb; 
 
     void Awake()
@@ -19,6 +26,18 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.01f, 0.3f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+
+        Debug.Log(Input.GetButtonDown("Jump"));
+        //isJumping = jumps.action.ReadValue<bool>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump") == true && !isGrounded)
+        {
+            Debug.Log("salto");
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
     }
 }
