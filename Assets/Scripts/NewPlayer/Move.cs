0be64674365891ed7689426b72ab1f67d54f829a
Gisela Benito <gisela.benito@enti.cat>
2023-11-20 13:51:54 +0100

    animation set walk

--- Assets/Scripts/NewPlayer/Move.cs
+++ Assets/Scripts/NewPlayer/Move.cs
@@ -10,6 +10,7 @@ public class Move : MonoBehaviour
     private Vector2 oldPosition = Vector2.zero; 
 
     [SerializeField] private Rigidbody2D physics;
+    [SerializeField] private Animator anim; 
 
     [Space]
     [Header("Velocity")]
@@ -28,6 +29,8 @@ public class Move : MonoBehaviour
 
     private void Walk() 
     {
+       
+
         if(playerStats.knockback)
         {
             physics.velocity = new Vector2(playerStats.knockbackVel, physics.velocity.y);
@@ -36,11 +39,13 @@ public class Move : MonoBehaviour
         {
             if (_direction.magnitude > 0 && currentSpeed >= 0)
             {
+                anim.SetBool("isMoving", true);
                 oldPosition = _direction;
                 currentSpeed += acceleraiton * maxSpeed * Time.fixedDeltaTime;
             }
             else
             {
+                anim.SetBool("isMoving", false);
                 currentSpeed -= deacceleraiton * maxSpeed * Time.fixedDeltaTime;
             }
 
