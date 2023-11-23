121f03559f90c31a654f70b8aedfd4f1f8dabd02
JAN PLANES MAZCUÑAN <jan.planes@enti.cat>
2023-11-23 19:05:36 +0100

    LAser shoots, destroys himself & object it collides with

--- Assets/laserBehaviour.cs
+++ Assets/laserBehaviour.cs
@@ -7,7 +7,8 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 public class laserBehaviour : MonoBehaviour
 {
     public float Speed = 4.5f;
-    
+    private Enemy enemy;
+    private FlyingEnemyDamage flyingEnemy;
     [SerializeField] InputActionReference laser;
     // Update is called once per frame
     void Update()
@@ -19,11 +20,17 @@ public class laserBehaviour : MonoBehaviour
        
         
         transform.Translate(Vector2.right * Time.deltaTime * Speed);
-        
 
+        StartCoroutine(DestroyLaser());
+    }
+    private IEnumerator DestroyLaser()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Destroy(gameObject);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Destroy(collision.gameObject);
         Destroy(gameObject);
     }
 }
