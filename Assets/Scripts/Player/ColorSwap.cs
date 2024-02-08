547a0ac9ef45e0a473daad79eefc2675ab512b86
Adrián <adrian.fernandez@enti.cat>
2024-02-08 14:40:31 +0100

    Jump working

--- Assets/Scripts/Player/ColorSwap.cs
+++ Assets/Scripts/Player/ColorSwap.cs
@@ -13,7 +13,7 @@ public class ColorSwap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        myRenderer = GetComponent<SpriteRenderer>();
+        myRenderer = GetComponentInChildren<SpriteRenderer>();
         newColor.a = 1;
         myRenderer.color = newColor;
 
