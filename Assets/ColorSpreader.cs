064e891e38186014a93f7bed7203f73a1c11de9c
Gisela Benito Izquierdo <gisela.benito@enti.cat>
2024-02-08 16:54:39 +0100

    Color Reciever correction

--- Assets/ColorSpreader.cs
+++ Assets/ColorSpreader.cs
@@ -37,14 +37,14 @@ public class ColorList
 public class ColorSpreader : MonoBehaviour
 {
     public ColorDatabase colorDatabase; // Referencia al ScriptableObject
-   [SerializeField] private Tilemap _spriteRenderer;
-   
+    [SerializeField] private Tilemap _spriteRenderer;
+
     public ColorTypes colorType; // Define el tipo de color a usar
 
 
     private void Start()
     {
-       
+
         //_spriteRenderer = GetComponent<Tilemap>();
         // Buscar el color correspondiente en la base de datos y asignarlo
         ColorProperty colorProperty = colorDatabase.colorList.Find(c => c.colorScheme == colorType);
