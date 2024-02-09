4e3b3319a670ea2379eb4656bc11840e86567ed5
Adrián <adrian.fernandez@enti.cat>
2024-02-09 14:17:15 +0100

    Ladders working with player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -26,6 +26,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private bool canDoubleJump = false;
     private float slide;
 
+    bool onStairs;
+
 
 
     void Start()
@@ -34,13 +36,28 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         ground = GetComponentInChildren<PlayerGroundDetection>();
         isJumping = false;
+        onStairs = false;
         coyoteTime = 0.3f;
     }
 
     void Update()
     {
         Walk();
+
+        if(onStairs)
+        {
+            Stairs();
+            rb.gravityScale = 0f;
+
+        }
+        else
+        {
+            rb.gravityScale = 9.81f;
+
+        }
+
         Jump();
+
     }
 
 
@@ -96,6 +113,8 @@ public class PlayerMovement : MonoBehaviour
 
         if (actualCoyoteTime > 0 && Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
+            rb.gravityScale = 9.81f;
+
             actualCoyoteTime = 0f;
             isJumping = true;
             actualJumpTimeCounter = jumpTimeCounter;
@@ -129,4 +148,36 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void Stairs()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+        else if(Input.GetKey(KeyCode.S)) 
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, -5);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ladder")
+        {
+            onStairs = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onStairs = false;
+    }
 }
