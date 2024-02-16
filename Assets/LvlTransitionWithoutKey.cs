bb86dcbdb0f9bc1eedc58664f25508081e8eae0b
Gisela Benito Izquierdo <gisela.benito@enti.cat>
2024-02-16 14:17:08 +0100

    Doors added all levels

---
+++ Assets/LvlTransitionWithoutKey.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlTransitionWithoutKey : MonoBehaviour
+{
+    public Animator transitions;
+    [SerializeField] public Scene currentScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //transitions.SetTrigger("LvlPassed");
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("LvlPassed");
+        }
+
+    }
+}
