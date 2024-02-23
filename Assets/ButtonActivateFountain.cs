79a4a5ed2f3a053a1bd3b9e7e1a71be594340096
JanPlanes <jan.planes@enti.cat>
2024-02-23 17:37:52 +0100

    All buttons have pressing animations

--- Assets/ButtonActivateFountain.cs
+++ Assets/ButtonActivateFountain.cs
@@ -5,18 +5,23 @@ public class ButtonActivateFountain : MonoBehaviour
 {
     public GrayZoneData grayDatabase;
     [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private GameObject fountain;
+    [SerializeField] private Animator pressed;
 
     public void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
-
+        pressed = GetComponent<Animator>();
+        fountain.SetActive(false);
     }
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("PuzzleBox"))
         {
-            grayDatabase.dashActive = true;
+            pressed.SetBool("Pressed", true);
+            fountain.SetActive(true);
+            //grayDatabase.dashActive = true;
         }
     }
    
--- Assets/Scripts/leverActivation.cs
+++ Assets/Scripts/leverActivation.cs
@@ -6,17 +6,20 @@ public class leverActivation : MonoBehaviour
     public bool isActive;
     [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private GameObject boxUI;
+    [SerializeField] private Animator pressed;
 
     private void Start()
     {
         isActive = false;
         sprite = GetComponent<SpriteRenderer>();
+        pressed = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("PuzzleBox"))
         {
+            pressed.SetBool("Pressed", true);
             Toggle();
         }
     }
