247d44af90a5063a0de10e189f78b1553a44d250
JAN PLANES MAZCUÑAN <jan.planes@enti.cat>
2024-03-15 02:14:31 +0100

    Buttons to flip and stop added

--- Assets/Scripts/Conveyor Belt/ConveyerLogic.cs
+++ Assets/Scripts/Conveyor Belt/ConveyerLogic.cs
@@ -8,32 +8,45 @@ public class ConveyerLogic : MonoBehaviour
     [SerializeField] private bool flip;
     [SerializeField] private Sprite startSprite;
     [SerializeField] private Sprite endSprite;
+    [SerializeField] private StopConveyor stopConveyor;
+    [SerializeField] private FlipConveyor flipConveyor;
 
     private BoxCollider2D col;
+    private float dt;
     private SpriteRenderer sp;
-    private PlatformEffector2D platform;
-
 
     // Start is called before the first frame update
     void Start()
     {
         col = GetComponent<BoxCollider2D>();
         sp = GetComponentInChildren<SpriteRenderer>();
-
+       
         col.size = new Vector2(sp.size.x, sp.size.y);
         
     }
-
+    private void Update()
+    {
+        if (stopConveyor.GetIsACtive())
+        {
+            dt = 0;
+        }
+        else
+        {
+            dt = Time.deltaTime;
+        }
+        flip = flipConveyor.GetIsACtive();
+        
+    }
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (flip)
         {
-            collision.transform.localPosition += Vector3.right * velocity * Time.deltaTime;
+            collision.transform.localPosition += Vector3.right * velocity * dt;
 
         }
         else
         {
-            collision.transform.localPosition += Vector3.left * velocity * Time.deltaTime;
+            collision.transform.localPosition += Vector3.left * velocity * dt;
 
         }
 
