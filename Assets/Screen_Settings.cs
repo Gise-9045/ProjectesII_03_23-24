9fc65d0d99a05ad42dd29797d0b0926c267ac743
Gisela Benito Izquierdo <gisela.benito@enti.cat>
2024-04-07 03:24:12 +0200

    Add full screen (not done yet)

--- Assets/Screen_Settings.cs
+++ Assets/Screen_Settings.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Screen_Settings : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +14,10 @@ public class Screen_Settings : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
